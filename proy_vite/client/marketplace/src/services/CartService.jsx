4ac224e0e98e0bc810047342536b5737d6a73103
Rocco Dell Amico <rdellamico@uade.edu.ar>
2024-10-19 13:49:34 -0300

    agrego metodos al cartservice (todavia faltan agregar add, remove, getTotalAmount, getTotalCartItem)

--- proy_vite/client/marketplace/src/services/CartService.jsx
+++ proy_vite/client/marketplace/src/services/CartService.jsx
@@ -5,6 +5,44 @@ const CART_BASE_URL = "http://localhost:4002/api"
 
 class CartService {
 
+    async createCart(email) {
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            };
+            const response = await axios.post(`${CART_BASE_URL}/user/carts/${email}`, null, config);
+            return response;
+        } catch (error) {
+            console.error("Error creating cart", error);
+            throw error;
+        }
+    }
+
+    async getCartById(userId) {
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+            const config = {
+                headers: {
+                  Authorization: `Bearer ${token}`,  // Añadir el token en el encabezado
+                }
+            };
+            const response = await axios.get(`${CART_BASE_URL}/user/carts/${userId}`, config);
+            return response; 
+        } catch(error){
+            console.error("Error fetching cart", error);
+            throw error;
+        }
+    }
+
     async getCartsAdmin() {
         try {
             const token = localStorage.getItem('token');
