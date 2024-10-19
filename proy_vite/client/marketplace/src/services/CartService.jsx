a000b16a49293362baf89830c939f9f8e7eceda2
Rocco Dell Amico <rdellamico@uade.edu.ar>
2024-10-19 18:54:45 -0300

    agrego todos los metodos del cartService

--- proy_vite/client/marketplace/src/services/CartService.jsx
+++ proy_vite/client/marketplace/src/services/CartService.jsx
@@ -61,6 +61,180 @@ class CartService {
             throw error;
         }
     }
+
+    async addProductToCart(){
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            };
+
+            const body = {
+                cartId: cartId,
+                productId: productId,
+                size: size,
+                quantity: quantity
+            }
+
+            const response = await axios.put(`${CART_BASE_URL}/user/carts`, body, config);
+            return response;
+        } catch (error) {
+            console.error("Error adding product to cart", error);
+            throw error;
+        }
+    }
+
+    async addOneProduct(cartId, productId, size){
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+    
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+    
+            const response = await axios.put(`${CART_BASE_URL}/user/carts/${cartId}/products/${productId}/${size}/addOne`, null, config);
+            return response.data; // Devuelve los datos de la respuesta
+        } catch (error) {
+            console.error("Error adding one product to cart", error);
+            throw error;
+        }
+    }
+
+    async substractOneProduct(cartId, productId, size){
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+    
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+    
+            const response = await axios.put(`${CART_BASE_URL}/user/carts/${cartId}/products/${productId}/${size}/substractOne`, null, config);
+            return response.data; // Devuelve los datos de la respuesta
+        } catch (error) {
+            console.error("Error subtracting one product to cart", error);
+            throw error;
+        }
+    }
+
+    async clearCart(cartId){
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+    
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+    
+            const response = await axios.put(`${CART_BASE_URL}/user/carts/${cartId}/clear`, null, config);
+            return response.data; // Devuelve los datos de la respuesta
+        } catch (error) {
+            console.error("Error clearing cart", error);
+            throw error;
+        }
+    }
+
+    async getTotal(cartId){
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+    
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+    
+            const response = await axios.get(`${CART_BASE_URL}/user/carts/${cartId}/getTotal`, null, config);
+            return response.data; // Devuelve los datos de la respuesta
+        } catch (error) {
+            console.error("Error getting total cart", error);
+            throw error;
+        }
+    }
+
+    async getItemCount(cartId){
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+    
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+    
+            const response = await axios.get(`${CART_BASE_URL}/user/carts/${cartId}/itemCount`, null, config);
+            return response.data; // Devuelve los datos de la respuesta
+        } catch (error) {
+            console.error("Error getting item count of cart", error);
+            throw error;
+        }
+    }
+
+    async getCartProducts(cartId){
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+    
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+    
+            const response = await axios.put(`${CART_BASE_URL}/user/carts/${cartId}/cartProducts`, null, config);
+            return response.data; // Devuelve los datos de la respuesta
+        } catch (error) {
+            console.error("Error getting cart products", error);
+            throw error;
+        }
+    }
+
+    async addDiscountCode(cartId, discountCode){
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found. Please login again.');
+            }
+    
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+    
+            const response = await axios.put(`${CART_BASE_URL}/user/carts/add-discount/${discountCode}/cart/${cartId}`, null, config);
+            return response.data; // Devuelve los datos de la respuesta
+        } catch (error) {
+            console.error("Error getting cart products", error);
+            throw error;
+        }
+    }
 }
 
 export default new CartService();
