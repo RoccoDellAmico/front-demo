5d6a0e9ca65876be569bef69d16e59e2a227de86
Rocco Dell Amico <rdellamico@uade.edu.ar>
2024-11-23 01:23:02 -0300

    agrego Banners

--- proy_vite/client/marketplace/src/App.jsx
+++ proy_vite/client/marketplace/src/App.jsx
@@ -11,6 +11,9 @@ import Footer from './components/Footer/Footer';
 import men_banner from './assets/banner_mens.png';
 import women_banner from './assets/banner_women.png';
 import kid_banner from './assets/banner_kids.png';
+import new_men_banner from './assets/BANNER-MEN.png';
+import new_women_banner from './assets/BANNER-WOMEN.png';
+import new_kids_banner from './assets/BANNER-KIDS.png';
 import './App.css';
 import Login from './Pages/LogIn';
 import CheckOut from './Pages/CheckOut';
@@ -27,9 +30,9 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/men" element={<ShopCategory banner={men_banner} category="MEN"/>} />
-        <Route path="/women" element={<ShopCategory banner={women_banner} category="WOMEN"/>} />
-        <Route path="/kids" element={<ShopCategory banner={kid_banner} category="KIDS"/>} />
+        <Route path="/men" element={<ShopCategory banner={new_men_banner} category="MEN"/>} />
+        <Route path="/women" element={<ShopCategory banner={new_women_banner} category="WOMEN"/>} />
+        <Route path="/kids" element={<ShopCategory banner={new_kids_banner} category="KIDS"/>} />
         <Route path="/aboutUs" element={<AboutUs/>} />
         <Route path="/cart" element={<Cart/>} />
         <Route path="/signUp" element={<LoginSignup/>} />
--- proy_vite/client/marketplace/src/Pages/Home.jsx
+++ proy_vite/client/marketplace/src/Pages/Home.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchProducts } from '../redux/ProductSlice';
 import FilterRedux from '../components/Filter/FilterRedux';
+import banner_home from '../assets/BANNER-HOME.png';
 import Filter from '../components/Filter/Filter';
+import './CSS/Home.css';
 
 
 const Home = () => {
@@ -14,6 +16,7 @@ const Home = () => {
 
     return (
         <div>
+            <img className='banner' src={banner_home} alt="" />
             <FilterRedux />
             {/*<Filter />*/}
             {/*<Popular/>*/}
